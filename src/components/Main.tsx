3773c6d9f72d94c9b78dedb8581cae6d0a55eee6
Zain Jaafar <zainjaafar120@gmail.com>
2024-11-01 11:27:37 +1300

    added basic layout and dummy info

---
+++ src/components/Main.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+    return (
+        <div className="ImageContainer">
+            <img src="mars.png" alt="mar-demo-picture" className="bgImage"/> 
+        </div>
+    )
+}
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,11 @@
+export default function SideBar() {
+    return (
+        <div>
+            <h2>The Brutal Martian Landscape</h2>
+            <div>
+                <p>Description</p>
+                <p>asdfaskdfjalks djfaskdjfla skdjf asdf jalsf jlkadf</p>
+            </div>
+        </div>
+    );
+}

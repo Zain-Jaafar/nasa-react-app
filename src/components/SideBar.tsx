fec5d14dfce010661d1a8547e38edd9f174320cb
Zain Jaafar <zainjaafar120@gmail.com>
2024-11-01 15:17:06 +1300

    made sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,13 @@
 export default function SideBar() {
     return (
-        <div>
-            <h2>The Brutal Martian Landscape</h2>
-            <div>
-                <p>Description</p>
-                <p>asdfaskdfjalks djfaskdjfla skdjf asdf jalsf jlkadf</p>
+        <div className="sidebar">
+            <div className="bgOverlay"></div>
+            <div className="sidebarContents">
+                <h2>The Brutal Martian Landscape</h2>
+                <div>
+                    <p>Description</p>
+                    <p>asdfaskdfjalks djfaskdjfla skdjf asdf jalsf jlkadf</p>
+                </div>
             </div>
         </div>
     );
